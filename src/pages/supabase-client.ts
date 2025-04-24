8ed6b40e1bd7c3b7ae8d210362af61bb346d6cf2
Chaitanya Jambhulkar <chaitanyajambhulkar768@gmail.com>
2025-04-24 23:53:25 +0530

    safety

--- src/pages/supabase-client.ts
+++ src/pages/supabase-client.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const PUBLIC_URL = "https://eyvladpojbssdagheujo.supabase.co";
-const PUBLIC_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5dmxhZHBvamJzc2RhZ2hldWpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0NjkwMTIsImV4cCI6MjA2MTA0NTAxMn0.IKpGrL1C5J13L36uPxd0JLrr5_hTEZlTtIs85zi3GGU";
-export const supabase = createClient(PUBLIC_URL, PUBLIC_KEY);
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
